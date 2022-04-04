5dbc10b85a75410e6cbeb2ddb1ed806f6395b885
Petr Filaretov <petr.filaretov@gmail.com>
2022-04-04 14:35:13 +0300

    initial commit

---
+++ src/main/java/pro/filaretov/spring/cloud/feign/springcloudopenfeignsample/SpringCloudOpenFeignSampleApplication.java
@@ -0,0 +1,13 @@
+package pro.filaretov.spring.cloud.feign.springcloudopenfeignsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOpenFeignSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudOpenFeignSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/filaretov/spring/cloud/feign/springcloudopenfeignsample/SpringCloudOpenFeignSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.filaretov.spring.cloud.feign.springcloudopenfeignsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudOpenFeignSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
